2eac5457eb72b790946cabf79064cc17111bed0b
SandroCy6 <sandrodiazzt@gmail.com>
2025-10-27 01:12:09 -0500

    Avance Backend

--- backend/src/main/java/com/viajafacil/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/viajafacil/backend/config/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-//abc
+
 @Configuration
+//Indica que esta clase contiene configuraciones de Spring (se cargará al iniciar la app).
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // Crea la configuración de seguridad principal
         http
                 .csrf(csrf -> csrf.disable()) // Desactiva CSRF (útil en APIs)
                 .authorizeHttpRequests(auth -> auth
--- backend/src/main/java/com/viajafacil/backend/controller/HelloController.java
+++ backend/src/main/java/com/viajafacil/backend/controller/HelloController.java
@@ -2,11 +2,12 @@ package com.viajafacil.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//hola :v
+
 @RestController
 public class HelloController {
+
     @GetMapping("/hello")
     public String hello() {
-        return "¡Hola, el backend Spring Boot ESTAMOS EN PROCESO";
+        return "¡Hola, el backend Spring Boot ESTAMOS EN PROCESO , PERO YA TENEMOS CONEXION A LA BSE DE DATOS! ";
     }
 }
